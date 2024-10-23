12e474f5f80fcbff6640406ad74c154532fc1dc6
Dhanushkumar <dhanushsrs@gmail.com>
2024-10-23 10:47:41 +0530

    updated code

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const transactionRoutes = require("./routes/transactions");
+const transactionRoutes = require("./src/routes/transactions");
 
 const app = express();
 
